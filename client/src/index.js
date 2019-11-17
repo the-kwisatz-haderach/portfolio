4f19c5e52e866f96b9adf9bad58f213ef5307f1c
Storchillarn <gurr_gurr@hotmail.com>
2019-11-17 17:24:12 +0100

    update path to fonts

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,5 @@ import './assets/images/black-texture.png'
 import './assets/images/silver-metal.jpg'
 import './assets/images/pipe.jpeg'
 import './favicon.ico'
-import './assets/fonts/Calculator.ttf'
 
 ReactDOM.render(<App />, document.getElementById('app'))
