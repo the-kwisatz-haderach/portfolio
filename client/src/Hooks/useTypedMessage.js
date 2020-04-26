5228a7e0c6f88db0aa6bc7ad6c7bfc69c3658e2b
Storchillarn <gurr_gurr@hotmail.com>
2020-04-26 18:32:04 +0200

    update typed message hook

--- client/src/Hooks/useTypedMessage.js
+++ client/src/Hooks/useTypedMessage.js
@@ -1,27 +1,34 @@
 import { useState, useEffect, useRef } from 'react'
 
-const useTypedMessage = (message = '', interval = 100) => {
+const useTypedMessage = (message = '', interval = 100, startTyping = true) => {
   const [typedMessage, setTypedMessage] = useState('')
   const originalMessage = useRef(message)
   const messageIndex = useRef(0)
   const characters = [...originalMessage.current]
 
   useEffect(() => {
-    const addIncrementally = setInterval(() => {
-      if (messageIndex.current < characters.length) {
-        setTypedMessage(
-          (currentMessage) => currentMessage + characters[messageIndex.current]
-        )
-        messageIndex.current += 1
-      }
-    }, interval)
+    let addIncrementally
+    if (startTyping) {
+      addIncrementally = setInterval(() => {
+        if (messageIndex.current < characters.length) {
+          setTypedMessage(
+            (currentMessage) =>
+              currentMessage + characters[messageIndex.current]
+          )
+          messageIndex.current += 1
+        }
+        if (messageIndex.current >= characters.length) {
+          clearInterval(addIncrementally)
+        }
+      }, interval)
+    }
 
     return () => {
       messageIndex.current = 0
       originalMessage.current = message
       clearInterval(addIncrementally)
     }
-  }, [message])
+  }, [message, startTyping])
 
   return typedMessage
 }
