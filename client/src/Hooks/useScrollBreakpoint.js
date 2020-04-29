fe0130fe1ad7f5e9261457e8d8af46868008e03d
Storchillarn <gurr_gurr@hotmail.com>
2020-04-29 20:25:04 +0200

    Create hook for handling scroll breakpoints.

---
+++ client/src/Hooks/useScrollBreakpoint.js
@@ -0,0 +1,52 @@
+import { useEffect, useCallback, useState } from 'react'
+import { throttle } from 'lodash'
+
+const defaultOptions = {
+  onReachBreakpoint: null,
+  throttleTime: 200,
+  removeListenerOnReach: false,
+  reset: false,
+}
+
+const useScrollBreakpoint = (breakpoint = 0, options = defaultOptions) => {
+  const [activeBreakpoint, setActiveBreakpoint] = useState(null)
+  const [reachedBreakpoint, setReachedBreakpoint] = useState(false)
+
+  useEffect(() => {
+    setActiveBreakpoint(
+      typeof breakpoint === 'function' ? breakpoint() : breakpoint
+    )
+  }, [breakpoint])
+
+  const checkBreakpoint = useCallback(
+    throttle(
+      (e) => {
+        if (window.scrollY >= activeBreakpoint) {
+          if (options.removeListenerOnReach) {
+            window.removeEventListener('scroll', checkBreakpoint)
+          }
+          !reachedBreakpoint && setReachedBreakpoint(true)
+          options.onReachBreakpoint && options.onReachBreakpoint(e)
+        }
+        if (options.reset && window.scrollY < activeBreakpoint) {
+          setReachedBreakpoint(false)
+        }
+      },
+      options.throttleTime >= 50 ? options.throttleTime : 200
+    ),
+    [activeBreakpoint, reachedBreakpoint]
+  )
+
+  useEffect(() => {
+    window.addEventListener('load', checkBreakpoint)
+    window.addEventListener('scroll', checkBreakpoint)
+    return () => {
+      window.removeEventListener('scroll', checkBreakpoint)
+      window.removeEventListener('load', checkBreakpoint)
+    }
+  }, [activeBreakpoint])
+
+  return reachedBreakpoint
+}
+
+export default useScrollBreakpoint
