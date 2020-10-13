477d0814ce9575943f771985a8ca9728cd7f4c8c
Gustaf Lundström <Gustaf.Lundstrom@systecon.se>
2020-10-13 21:26:20 +0200

    move Timeline style to separate file

--- client/src/Components/Timeline/Timeline.js
+++ client/src/Components/Timeline/Timeline.js
@@ -1,127 +1,20 @@
 import React, { useEffect } from 'react'
-import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import DefaultEntry from './Entry'
 import useElementScrollTop from '../../Hooks/useElementScrollTop'
 import DefaultDateMarker from './DateMarker'
 import Skill from './Skill'
 import getAnimationClassname from '../../utils/getAnimationClassname'
-import { Header } from './Entry'
-
-const Container = styled.div`
-  width: 100%;
-  display: flex;
-  flex-flow: row wrap;
-  position: relative;
-`
-
-const Line = styled.span`
-  width: 4px;
-  height: 100%;
-  background-color: #0000001f;
-  position: absolute;
-  top: 0;
-  z-index: 1;
-  left: calc(50% - 2px);
-  &::before {
-    content: '';
-    position: absolute;
-    top: -5px;
-    left: calc(50% - 15px);
-    width: 30px;
-    height: 5px;
-    background-color: #0000001f;
-  }
-  &::after {
-    content: '';
-    position: absolute;
-    bottom: -5px;
-    left: calc(50% - 15px);
-    width: 30px;
-    height: 5px;
-    background-color: #0000001f;
-  }
-`
-
-const ConnectingLine = styled.div`
-  position: absolute;
-  top: calc(50% - 2px);
-  right: -50px;
-  left: unset;
-  z-index: -1;
-  height: 50%;
-  border-top: 4px solid #0000000d;
-  width: 0px;
-  transform-origin: right;
-`
-
-const SkillsList = styled.div`
-  display: flex;
-  width: 100%;
-  justify-content: space-around;
-`
-
-const TimelineColumn = styled.div`
-  position: relative;
-  flex: 1;
-  &:nth-child(2) {
-    flex: none;
-    width: 100px;
-    margin: 0 50px;
-  }
-`
-
-const TimelineRow = styled.div`
-  overflow: hidden;
-  width: 100%;
-  padding: 7em ${props => props.theme.padding.horizontal.medium};
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  position: relative;
-  z-index: 0;
-  margin-bottom: 1em;
-  &:first-of-type {
-    margin-top: 70px;
-  }
-  &:last-of-type {
-    margin-bottom: 70px;
-  }
-  &:nth-child(odd) {
-    flex-direction: row-reverse;
-    background-color: ${props => props.theme.colors.grey};
-    & ${ConnectingLine} {
-      left: -50px;
-      right: unset;
-      transform-origin: left;
-    }
-    & ${Header} {
-      justify-content: flex-end;
-    }
-    & ${TimelineColumn} {
-      & img {
-        order: 1;
-      }
-      & * {
-        text-align: right;
-      }
-    }
-  }
-`
-
-const TimelineStart = styled.div`
-  width: 100%;
-  text-align: center;
-  position: sticky;
-  top: 55%;
-  z-index: 1;
-`
-
-const TimelineSticky = styled.i`
-  font-size: 5em;
-  position: relative;
-  top: 8px;
-`
+import {
+  TimelineColumn,
+  TimelineRow,
+  TimelineStart,
+  TimelineSticky,
+  ConnectingLine,
+  Container,
+  Line,
+  SkillsList
+} from './style'
 
 export default function Timeline({
   entries = [],
@@ -140,16 +33,38 @@ export default function Timeline({
     1.0,
     '0px 0px -20% 0px'
   )
+  const [fourthRowRef, reachedFourthEntry] = useElementScrollTop(
+    1.0,
+    '0px 0px -20% 0px'
+  )
+  const [fifthRowRef, reachedFifthEntry] = useElementScrollTop(
+    1.0,
+    '0px 0px -20% 0px'
+  )
+  const [sixthRowRef, reachedSixthEntry] = useElementScrollTop(
+    1.0,
+    '0px 0px -20% 0px'
+  )
   const [stickyRef, reachedStickyRef] = useElementScrollTop(
     1.0,
     '0px 0px -40% 0px'
   )
 
-  const rowRefs = [firstRowRef, secondtRowRef, thirdRowRef]
+  const rowRefs = [
+    firstRowRef,
+    secondtRowRef,
+    thirdRowRef,
+    fourthRowRef,
+    fifthRowRef,
+    sixthRowRef
+  ]
   const rowRefStatus = [
     reachedFirstEntry,
     reachedSecondEntry,
-    reachedThirdEntry
+    reachedThirdEntry,
+    reachedFourthEntry,
+    reachedFifthEntry,
+    reachedSixthEntry
   ]
 
   const furthestRowReached = rowRefStatus.reduce(
---
+++ client/src/Components/Timeline/style.js
@@ -0,0 +1,117 @@
+import styled from 'styled-components'
+import { Header } from './Entry'
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-flow: row wrap;
+  position: relative;
+`
+
+export const Line = styled.span`
+  width: 4px;
+  height: 100%;
+  background-color: #0000001f;
+  position: absolute;
+  top: 0;
+  z-index: 1;
+  left: calc(50% - 2px);
+  &::before {
+    content: '';
+    position: absolute;
+    top: -5px;
+    left: calc(50% - 15px);
+    width: 30px;
+    height: 5px;
+    background-color: #0000001f;
+  }
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: -5px;
+    left: calc(50% - 15px);
+    width: 30px;
+    height: 5px;
+    background-color: #0000001f;
+  }
+`
+
+export const ConnectingLine = styled.div`
+  position: absolute;
+  top: calc(50% - 2px);
+  right: -50px;
+  left: unset;
+  z-index: -1;
+  height: 50%;
+  border-top: 4px solid #0000000d;
+  width: 0px;
+  transform-origin: right;
+`
+
+export const SkillsList = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-around;
+`
+
+export const TimelineColumn = styled.div`
+  position: relative;
+  flex: 1;
+  &:nth-child(2) {
+    flex: none;
+    width: 100px;
+    margin: 0 50px;
+  }
+`
+
+export const TimelineRow = styled.div`
+  overflow: hidden;
+  width: 100%;
+  padding: 7em ${props => props.theme.padding.horizontal.medium};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+  z-index: 0;
+  margin-bottom: 1em;
+  &:first-of-type {
+    margin-top: 70px;
+  }
+  &:last-of-type {
+    margin-bottom: 70px;
+  }
+  &:nth-child(odd) {
+    flex-direction: row-reverse;
+    background-color: ${props => props.theme.colors.grey};
+    & ${ConnectingLine} {
+      left: -50px;
+      right: unset;
+      transform-origin: left;
+    }
+    & ${Header} {
+      justify-content: flex-end;
+    }
+    & ${TimelineColumn} {
+      & img {
+        order: 1;
+      }
+      & * {
+        text-align: right;
+      }
+    }
+  }
+`
+
+export const TimelineStart = styled.div`
+  width: 100%;
+  text-align: center;
+  position: sticky;
+  top: 55%;
+  z-index: 1;
+`
+
+export const TimelineSticky = styled.i`
+  font-size: 5em;
+  position: relative;
+  top: 8px;
+`
